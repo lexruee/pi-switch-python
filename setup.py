02c6176f082dddbcb31da3431d01e94a78b34d3c
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2015-01-30 10:58:54 +0100

    bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.extension import Extension
 setup(
     name="pi_switch",
     packages = [ "pi_switch" ],
-    version = "0.1.5",
+    version = "0.2.0",
     description = "A library for the raspberry pi to control remote power outlet sockets.",
     author = "Alexander Rueedlinger",
     author_email = "a.rueedlinger@gmail.com",
@@ -14,7 +14,14 @@ setup(
     download_url = "https://github.com/lexruee/pi-switch-python/tarball/0.1.5",
     keywords = ['switch', 'outlet sockets', 'control', 'remote', 'raspberry pi'],
     classifiers = [
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)"
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: Other OS",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Hardware",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Other Environment"
     ],
     ext_modules = [
         Extension("pi_switch_wrapper",
@@ -23,5 +30,12 @@ setup(
                     ],
                     libraries = ["boost_python", "wiringPi"])
     ],
-    install_requires = [],
-    )
+    long_description = """\
+Pi Switch
+-------------------------------------
+
+Pi Switch is a library for controlling 315/433MHz remote power outlet sockets.
+
+This library is a port of the Arduino rc-switch library for the Raspberry Pi and the Python Programming Language.
+"""
+)
