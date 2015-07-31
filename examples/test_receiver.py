2064ad91633431ecf7e370fdb9c7009dc9b2f6e8
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-07-31 04:27:47 +0200

    update test_receiver.py example

--- examples/test_receiver.py
+++ examples/test_receiver.py
@@ -13,13 +13,14 @@ receiver.enableReceive(2)
 num = 0
 
 while True:
-    received_value = receiver.getReceivedValue()
+    if receiver.available():
+        received_value = receiver.getReceivedValue()
 
-    if received_value:
-        num += 1
-        print("Received[%s]:" % num)
-        print(received_value)
-        print("%s / %s bit" % (received_value, receiver.getReceivedBitlength()))
-        print("Protocol: %s" % receiver.getReceivedProtocol())
-        print("")
-        receiver.resetAvailable()
+        if received_value:
+            num += 1
+            print("Received[%s]:" % num)
+            print(received_value)
+            print("%s / %s bit" % (received_value, receiver.getReceivedBitlength()))
+            print("Protocol: %s" % receiver.getReceivedProtocol())
+            print("")
+            receiver.resetAvailable()
